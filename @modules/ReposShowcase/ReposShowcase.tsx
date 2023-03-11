00e6ce9fefff8978b9ce2b00bec596f807cd3be2
Orwa Shams <orwa735@gmail.com>
2023-03-11 19:59:17 +0200

    Github API to get repos

---
+++ @modules/ReposShowcase/ReposShowcase.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+import { Octokit } from "octokit";
+import { type OctokitResponse } from "@octokit/types";
+import { type ReposType } from "@shared";
+
+const fetchRepos = async () => {
+  const octokit = new Octokit({
+    auth: process.env.GH_ACCESS_TOKEN,
+  });
+
+  const res: OctokitResponse<ReposType, number> = await octokit.request(
+    "GET /users/orwashams/repos"
+  );
+
+  return res.data;
+};
+
+export const ReposShowcase = () => {
+  const [response, setResponse] = useState<ReposType>();
+
+  useEffect(() => {
+    void (async () => {
+      const data = await fetchRepos();
+      setResponse(data);
+    })();
+  }, []);
+  return (
+    <>
+      <>{response}</>
+    </>
+  );
+};
