31cbeaf8b96b6e6dbba265623346a18c70535b8b
Orwa Shams <orwa.s.shams@gmail.com>
2023-09-16 21:04:30 +0300

    terminal progress

--- src/app/terminal/page.tsx
+++ src/app/terminal/page.tsx
@@ -5,13 +5,28 @@ import { useState } from "react";
 export default function Terminal() {
   const [value, setValue] = useState("");
   return (
-    <div className="w-full bg-[#140614] flex justify-center items-center min-h-screen flex-col p-4 ">
-      <textarea
-        className=" w-full  bg-black   text-green-500 font-bold text-2xl caret-white p-2  resize-none focus:outline-none  flex-grow border-box"
-        autoFocus
-        defaultValue={">"}
-        onChange={(e) => setValue(() => e.target.value)}
-      />
+    <div className=" bg-[#1b1b1b] flex justify-center items-center  ">
+      <div className="flex justify-center  min-h-screen  p-4 w-full">
+        <div className="flex h-full text-green-500 text-2xl p-2 bg-[#1b1b1b] font-bold">
+          root@orwa:~${" "}
+        </div>
+
+        <div className="w-full">
+          <input
+            className=" w-full bg-[#1b1b1b] decoration-transparent  text-green-500 font-bold text-2xl caret-white p-2  resize-none focus:outline-none border-box"
+            autoFocus
+            value={value}
+            spellCheck={false}
+            onChange={(e) => setValue(() => e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                console.log(value);
+                setValue("");
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
