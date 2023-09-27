6fd33d2de92af22fa7a60b96c3f32bebf753c22a
Orwa Shams <orwa.s.shams@gmail.com>
2023-09-27 23:18:02 +0300

    removed wip in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
+import Terminal from "./terminal/TeminalUtil";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  items-center justify-center">
-      <div>
-        <Image src="/logo.png" alt="logo" width={100} height={100} />
+    <main className="flex min-h-screen flex-col items-center justify-center ">
+      <div className="flex items-center justify-center gap-56 bg-red-700">
+        <div className="flex flex-col items-center justify-center bg-blue-600">
+          <Image
+            src="logo/brand04_white.svg"
+            alt="logo"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="bg-purple-500">
+          <Terminal />
+        </div>
       </div>
     </main>
   );
