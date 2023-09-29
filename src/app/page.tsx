135dc4508b5b3ac778a80d6de5c49ccaca2393d1
Orwa Shams <orwa.s.shams@gmail.com>
2023-09-29 14:55:49 +0300

    landing page taking shape, added VT323 font and fixed font centering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import { Terminal } from "./components/Terminal";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex ">
-        <div className=" h-[40rem] w-[40rem] flex items-center justify-center">
-          <Image
-            src="logo/brand04_white.svg"
-            alt="Orwa's brand logo"
-            width={100}
-            height={100}
-          />
+      <div className="flex">
+        <div className="h-[40rem] w-[40rem] flex flex-col gap-20">
+          <div className="flex justify-center">
+            <Image
+              src="logo/brand04_white.svg"
+              alt="Orwa's brand logo"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="text-6xl font-VT323 flex flex-col gap-20">
+            <p>Orwa I&apos;m a...Developer?!!</p>
+          </div>
         </div>
-
         <div className=" h-[40rem] w-[69rem]">
           <Terminal />
         </div>
